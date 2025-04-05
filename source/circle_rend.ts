f2f8e230b55a5dde3bea4d84f9b89a0392a5dfec
deltasampler <deltasampler@gmail.com>
2025-04-05 14:42:02 +0300

    update point rend and add circle rend

--- source/circle_rend.ts
+++ source/circle_rend.ts
@@ -15,16 +15,18 @@ const layout = layout_new();
 layout_attrib(layout, ATTRIB_TYPE.F32, 3);
 layout_attrib(layout, ATTRIB_TYPE.F32, 1);
 layout_attrib(layout, ATTRIB_TYPE.S32, 1);
+layout_attrib(layout, ATTRIB_TYPE.S32, 1);
+layout_attrib(layout, ATTRIB_TYPE.F32, 1);
 
-export class point_rdata_t {
+export class circle_rdata_t {
     data: ArrayBuffer;
     len: number;
     cap: number;
     instances: DataView[];
 };
 
-export function point_rdata_new(): point_rdata_t {
-    const rdata = new point_rdata_t();
+export function circle_rdata_new(): circle_rdata_t {
+    const rdata = new circle_rdata_t();
     rdata.data = new ArrayBuffer(0);
     rdata.len = 0;
     rdata.cap = 0;
@@ -33,7 +35,7 @@ export function point_rdata_new(): point_rdata_t {
     return rdata;
 }
 
-export function point_rdata_build(rdata: point_rdata_t, cap: number): void {
+export function circle_rdata_build(rdata: circle_rdata_t, cap: number): void {
     const data = new ArrayBuffer(cap * layout.stride);
     const instances: DataView[] = [];
 
@@ -47,24 +49,30 @@ export function point_rdata_build(rdata: point_rdata_t, cap: number): void {
     rdata.instances = instances;
 }
 
-export function point_rdata_instance(rdata: point_rdata_t, index: number, position: vec2_t, radius: number, zindex: number, color: vec4_t) {
+export function circle_rdata_instance(rdata: circle_rdata_t, index: number, position: vec2_t, radius: number, zindex: number, inner_color: vec4_t, outer_color: vec4_t, outline: number) {
     const instance = rdata.instances[index];
 
     instance.setFloat32(0, position[0], true);
     instance.setFloat32(4, position[1], true);
     instance.setFloat32(8, zindex, true);
     instance.setFloat32(12, radius, true);
-    instance.setInt32(16, vec4_bitpack256v(color), true);
+    instance.setInt32(16, vec4_bitpack256v(inner_color), true);
+    instance.setInt32(20, vec4_bitpack256v(outer_color), true);
+    instance.setFloat32(24, outline, true);
 };
 
-export function point_rend_init() {
+export function circle_rend_init() {
     program = gl_link_program({
         [gl.VERTEX_SHADER]: `#version 300 es
             layout(location = 0) in vec3 i_position;
             layout(location = 1) in float i_radius;
-            layout(location = 2) in int i_color;
+            layout(location = 2) in int i_inner_color;
+            layout(location = 3) in int i_outer_color;
+            layout(location = 4) in float i_outline;
             flat out float v_radius;
-            flat out int v_color;
+            flat out int v_inner_color;
+            flat out int v_outer_color;
+            flat out float v_outline;
             out vec2 v_tex_coord;
             uniform mat4 u_projection;
             uniform mat4 u_view;
@@ -88,14 +96,19 @@ export function point_rend_init() {
 
                 gl_Position = u_projection * u_view * vec4(position, i_position.z, 1.0);
                 v_radius = i_radius;
-                v_color = i_color;
+                v_inner_color = i_inner_color;
+                v_outer_color = i_outer_color;
+                v_outline = i_outline;
                 v_tex_coord = tex_coords[gl_VertexID];
             }
         `,
         [gl.FRAGMENT_SHADER]: `#version 300 es
             precision highp float;
             out vec4 o_frag_color;
-            flat in int v_color;
+            flat in float v_radius;
+            flat in int v_inner_color;
+            flat in int v_outer_color;
+            flat in float v_outline;
             in vec2 v_tex_coord;
 
             vec4 unpack256(int packed) {
@@ -110,12 +123,15 @@ export function point_rend_init() {
             void main() {
                 vec2 uv = v_tex_coord;
                 vec2 cp = uv * 2.0 - 1.0;
+                float l = length(cp);
+                vec4 inner_color = unpack256(v_inner_color);
+                vec4 outer_color = unpack256(v_outer_color);
+                float alias_width = fwidth(l);
+                float inner_mask = smoothstep(1.0 - v_outline / v_radius, 1.0 - alias_width, l);
+                vec4 color = mix(inner_color, outer_color, inner_mask);
+                color.w *= smoothstep(1.0, 1.0 - alias_width, l);
 
-                if (cp.x * cp.x + cp.y * cp.y > 1.0) {
-                    discard;
-                }
-
-                o_frag_color = unpack256(v_color);
+                o_frag_color = color;
             }
         `
     })!;
@@ -124,7 +140,7 @@ export function point_rend_init() {
     u_view = gl.getUniformLocation(program, "u_view")!;
 }
 
-export function point_rend_build(rdata: point_rdata_t) {
+export function circle_rend_build(rdata: circle_rdata_t) {
     vao = gl.createVertexArray();
     gl.bindVertexArray(vao);
 
@@ -135,7 +151,7 @@ export function point_rend_build(rdata: point_rdata_t) {
     layout_build_gl(layout, true);
 }
 
-export function point_rend_render(rdata: point_rdata_t, camera: cam2_t): void {
+export function circle_rend_render(rdata: circle_rdata_t, camera: cam2_t): void {
     gl.useProgram(program);
     gl.uniformMatrix4fv(u_projection, false, camera.projection);
     gl.uniformMatrix4fv(u_view, false, camera.view);
